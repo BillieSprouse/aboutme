04ce08cfd82cebc82f1330f0451d737bf4ea47a4
billiesprouse <billie.sprouse@gmail.com>
2018-09-16 17:47:45 -0700

    -a

--- js/app.js
+++ js/app.js
@@ -1,22 +1,19 @@
 'use strict';
 
 var correctAnswersTotal = 0;
-
+var name = '';
 
 function customizePrompt() {
-var name = prompt('What is your name?');
-console.log('CurrentUserName ' + name);
-alert('Hi ' + name + ' Welcome to Our Fun Guessing Game');
-alert("Please answer each question with a 'YES'/Y or 'NO'/N" );
-
+    name = prompt('What is your name?');
+    console.log('CurrentUserName ' + name);
+    alert('Hi ' + name + ' Welcome to Our Fun Guessing Game');
+    alert("Please answer each question with a 'YES'/Y or 'NO'/N" );
+    startPetOwned();
 }
 
 
-customizePrompt();
-
-
-function petOwned() {
-    prompt('Do I own a pet dragon?');
+function startPetOwned() {
+   var petOwned = prompt('Do I own a pet dragon?');
     petOwned = petOwned.toUpperCase();
     console.log('petOwned is ' + petOwned);
     if(petOwned === 'YES'|| petOwned === 'Y')
@@ -32,40 +29,17 @@ function petOwned() {
         alert('The answer is not valid. Please Enter YES or NO');
         console.log('petOwnedInvalid');
     }
+    startRunMileage();
 }
-customizePrompt();
-
-function petOwned() {
-prompt('Do I own a pet dragon?');
-petOwned = petOwned.toUpperCase();
-console.log('petOwned is ' + petOwned);
-if(petOwned === 'YES'|| petOwned === 'Y')
-    {
-    console.log('yesreg ' + petOwned);
-    alert('No, I am not Daenerys');
-}else if(petOwned === 'NO' || petOwned === 'N')
-    {
-    console.log('petOwnedNeg ' + petOwned);
-    alert('Sadly, that is the right answer');
-    correctAnswersTotal++;
-}else{
-    alert('The answer is not valid. Please Enter YES or NO');
-    console.log('petOwnedInvalid');
-}
-}
-
-petowned();
-
-function runMileage() {
- prompt('Have I ran over 50 milese in one day?');
-runMileage = runMileage.toUpperCase();
-console.log('runMileage is ' + runMileage);
-if(runMileage === 'YES'|| runMileage === 'Y')
-    {
+function startRunMileage() {
+    prompt('Have I ran over 50 milese in one day?');
+    runMileage = runMileage.toUpperCase();
+    console.log('runMileage is ' + runMileage);
+    if(runMileage === 'YES'|| runMileage === 'Y'){
     console.log('yesRegRunMileage ' + runMileage);
     alert('Keep guessing..');
 }else if(runMileage === 'NO' || runMileage === 'N')
-{
+    {
     console.log('negRegRunMileage ' + runMileage);
     alert("That's right I have not yet ran that far");
     correctAnswersTotal++;
@@ -73,62 +47,67 @@ if(runMileage === 'YES'|| runMileage === 'Y')
     alert('The answer is not valid. Please Enter YES or NO');
     console.log('runMileageInvalid');
 }
+    startMyColor();
 }
 
-var myColor = prompt('Do I like the color red?');
-myColor = myColor.toUpperCase();
-console.log('myColor is ' + myColor);
-if(myColor === 'YES'|| myColor === 'Y')
-    {
+function startMyColor() {
+    prompt('Do I like the color red?');
+    myColor = myColor.toUpperCase();
+    console.log('myColor is ' + myColor);
+    if(myColor === 'YES'|| myColor === 'Y'){
+   
     console.log('yesRegMyColor ' + myColor);
     alert('Not a fan of that color. Keep guessing..');
-}else if(runMileage === 'NO' || runMileage === 'N')
-{
-    console.log('negRegMyColor ' + myColor);
-    alert("Red is not my fave but pink is!");
-    correctAnswersTotal++;
-}else{
-    alert('The answer is not valid. Please Enter YES/Y or NO/Y');
-    console.log('runMileageInvalid');
+    }else if(runMileage === 'NO' || runMileage === 'N'){   
+        console.log('negRegMyColor ' + myColor);
+        alert("Red is not my fave but pink is!");
+        correctAnswersTotal++;
+    }else{
+        alert('The answer is not valid. Please Enter YES/Y or NO/Y');
+        console.log('runMileageInvalid');
+    }
+    startFlightHistory();
 }
 
-var flightHistory = prompt('Have I ever flown?');
-flightHistory = flightHistory.toUpperCase();
-console.log('flightHistory is ' + flightHistory);
-if(flightHistory === 'YES'|| flightHistory === 'Y')
-    {
-    console.log('yesRegFlightHIstory ' + flightHistory);
-    alert('Yep, I have flown too many times in my life,not a fan.');
-    correctAnswersTotal++;
-}else if(flightHistory === 'NO' || flightHistory === 'N')
-{
-    console.log('negRegFlightHistory ' + flightHistory);
-    alert("Wow, you are way off track. Keep guessing..");
-}else{
-    alert('The answer is not valid. Please Enter YES/Y or NO/N');
-    console.log('flightHistoryInvalid');
+function startFlightHistory () {
+    prompt('Have I ever flown?');
+    flightHistory = flightHistory.toUpperCase();
+    console.log('flightHistory is ' + flightHistory);
+    if(flightHistory === 'YES'|| flightHistory === 'Y'){
+        console.log('yesRegFlightHIstory ' + flightHistory);
+        alert('Yep, I have flown too many times in my life,not a fan.');
+        correctAnswersTotal++;
+    }else if(flightHistory === 'NO' || flightHistory === 'N'){
+        console.log('negRegFlightHistory ' + flightHistory);
+        alert("Wow, you are way off track. Keep guessing..");
+    }else{
+        alert('The answer is not valid. Please Enter YES/Y or NO/N');
+        console.log('flightHistoryInvalid');
+    }
+    startTripHistory();   
 }
 
-var tripHistory = prompt('Have I ever ventured to the land of fire and ice?');
-tripHistory = tripHistory.toUpperCase();
-console.log('tripHistory is ' + tripHistory);
-if(tripHistory === 'YES'|| tripHistory === 'Y')
-    {
-    console.log('yesRegTripHistory ' + tripHistory);
-    alert('Yep, I lived there for 2 years');
-    correctAnswersTotal++;
-}else if(tripHistory === 'NO' || tripHistory === 'N')
-{
-    console.log('negRegTripHistory ' + tripHistory);
-    alert('Too bad, you guessed wrong..');
-}else{
-    alert('The answer is not valid. Please Enter YES/Y or NO/N');
-    console.log('tripHistoryInvalid');
+function startTripHistory () {
+    prompt('Have I ever ventured to the land of fire and ice?');
+    tripHistory = tripHistory.toUpperCase();
+    console.log('tripHistory is ' + tripHistory);
+    if(tripHistory === 'YES'|| tripHistory === 'Y'){
+        console.log('yesRegTripHistory ' + tripHistory);
+        alert('Yep, I lived there for 2 years');
+        correctAnswersTotal++;
+    }else if(tripHistory === 'NO' || tripHistory === 'N'){
+        console.log('negRegTripHistory ' + tripHistory);
+        alert('Too bad, you guessed wrong..');
+    }else{
+        alert('The answer is not valid. Please Enter YES/Y or NO/N');
+        console.log('tripHistoryInvalid');
+    }
+    startNumberGuess();
 }
+    
 
-
-for(var i = 0; i < 4; i ++ ) {
-
+function startNumberGuess() {
+    for(var i = 0; i < 4; i ++ ) {
     console.log('trackingVarI ' + i );
     var numberGuess = prompt('How many marathons have I ran?');
     numberGuess = parseInt(numberGuess, 10);
@@ -149,12 +128,15 @@ for(var i = 0; i < 4; i ++ ) {
     }else{
         alert('Please, try again');
       
+        }
     }
+    startCountriesVisited();
 }
 
 var myAnswers = ['ICELAND', 'JAPAN','SOUTH KOREA','GREECE'];
-for(var i = 0; i < 6; i++ ) {
 
+function startCountriesVisited() {
+    for(var i = 0; i < 6; i++ ) {
     console.log('trackingVarI ' + i );
     var countriesVisited= prompt('Name some countries I have visited');
     countriesVisited = countriesVisited.toUpperCase();
@@ -174,9 +156,16 @@ for(var i = 0; i < 6; i++ ) {
 
     }
     alert('Hi ' + name + ' You have a score of ' + correctAnswersTotal);
+    }
+ 
 }
-    
 
+//customizePrompt();
+// petOwned();
+//runMileage();
+//myColor();
+//flightHistory();
+//tripHistory();
 
 
 
